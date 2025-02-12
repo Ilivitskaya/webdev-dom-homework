78b988cd0c64576a7bfa4e8b4eb328e9af94118d
Your Name <you@example.com>
2025-02-12 20:56:39 +0500

    разбила на модули, установила npm, Pretter, ESLint

---
+++ modules/renderComments.js
@@ -0,0 +1,30 @@
+import { comments } from "./comments.js";
+import { initClickLike, initClickComment, initAddComment } from "./initListeners.js"
+
+export const renderComments = () => {
+    const commentsList = document.querySelector(".comments");
+
+    commentsList.innerHTML = comments.map((comment, index) => {
+      return `<li data-index="${index}"  class="comment">
+        <div class="comment-header">
+          <div>${comment.name}</div>
+          <div>${comment.date}</div>
+        </div>
+        <div class="comment-body">
+          <div class="comment-text">
+            ${comment.text}
+          </div>
+        </div>
+        <div class="comment-footer">
+          <div class="likes">
+            <span class="likes-counter">${comment.likes}</span>
+            <button data-index="${index}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
+          </div>
+        </div>
+      </li>`
+    }).join("")
+
+    initClickLike();
+    initClickComment();
+    initAddComment();
+  }
